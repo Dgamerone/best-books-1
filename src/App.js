ea8c565fa4d199523d36a29407333e6bd00bd840
Dgamerone <granado.douglase@gmail.com>
2022-12-27 13:43:54 -0300

    pre-entrega coderhouse react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Route, Routes, BrowserRouter } from "react-router-dom";
+import Navbar from "./componentes/Navbar";
+import { Inicio } from "./paginas/Inicio";
+import Productos from "./paginas/Productos";
+import Contacto from "./paginas/Contacto";
+import Carrito from "./paginas/Carrito";
+import ItemListContainer from "./componentes/ItemListContainer";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Inicio />} />
+          <Route path="/productos" element={<Productos />} />
+          <Route path="/contacto" element={<Contacto />} />
+          <Route path="/carrito" element={<Carrito />} />
+        </Routes>
+      </BrowserRouter>
+
+      <ItemListContainer />
     </div>
   );
 }
---
+++ src/componentes/ItemListContainer/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const ItemListContainer = () => {
+  return (
+    <h1>Douglas Granado</h1>
+   
+  )
+}
+
+export default ItemListContainer
