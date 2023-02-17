26ad5490ad279f138ce1f60530d0f3f782c1b5ab
Dgamerone <granado.douglase@gmail.com>
2023-02-17 10:13:00 -0300

    Corrigiendo carpeta Itemcss

--- src/componentes/item/Item.jsx
+++ src/componentes/item/Item.jsx
@@ -1,5 +1,5 @@
 import ButtonUniversal from "../buttonUniversal/ButtonUniversal";
-import './item.css';
+import './tem.css';
 import React, {useState} from 'react';
 // import { useState } from 'react';
 import { Link } from 'react-router-dom';
